e9f0b3be6a1c7e96c1935c8f8467e8043760564e
grawish <grawish06@gmail.com>
2020-06-01 03:09:01 +0530

    saving some progress somewhat

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from home import models
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -11,12 +13,17 @@ def about(index):
     return render(index, 'home/about.html')
 
 
-def contact(index):
-    if index.method == 'POST':
-        name = index.POST.get('contname')
-        email = index.POST.get('contmail')
-        phone = index.POST.get('contno')
-        desc = index.POST.get('contdesc')
-        xcontact = models.Contact(name=name, email=email, contact=phone, desc=desc)
-        xcontact.save()
-    return render(index, 'home/contact.html')
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('contname')
+        email = request.POST.get('contmail')
+        phone = request.POST.get('contno')
+        desc = request.POST.get('contdesc')
+        if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(desc) < 4:
+            messages.error(request, 'Please fill the form correctly!')
+        else:
+            messages.success(request, 'Form has been filled successfully!')
+            xcontact = models.Contact(name=name, email=email, contact=phone, desc=desc)
+            xcontact.save()
+
+    return render(request, 'home/contact.html')
