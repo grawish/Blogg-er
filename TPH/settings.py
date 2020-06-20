20d47478c930eecc13454dd8569b35fa6f31c5be
Grawish <grawish06@gmail.com>
2020-06-21 02:16:39 +0530

    recaptcha integration

--- TPH/settings.py
+++ TPH/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
